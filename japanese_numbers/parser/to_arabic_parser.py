7cce3644991317e03707a97591957def04920ef6
Josh Meyer <joshuameyer@email.arizona.edu>
2018-04-17 10:35:42 -0500

    removed the results object... not we only get the list of arabic numbers themselves

--- japanese_numbers/parser/to_arabic_parser.py
+++ japanese_numbers/parser/to_arabic_parser.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- encoding:utf-8 -*-
 
-
-from japanese_numbers.result import ParsedResult
 from japanese_numbers.token import Tokenized, NUMERICS
 from japanese_numbers.kind import (  # noqa
   UNIT_KIND,
@@ -21,21 +19,22 @@ def _collect_numerics(val, pos):
   return int(''.join(stack)), len(stack)
 
 
-def to_arabic(val, encode='utf8'):
+def to_arabic(val, encode='utf8'):  
   stacks, numbers, texts, analyzing, index = ([], [], [], False, -1)
   results = []
 
+  # function for collecting results
   def _append_result():
-    results.append(ParsedResult(text=''.join(texts),
-                                number=sum(stacks) + sum(numbers),
-                                index=index))
+    results.append(sum(stacks) + sum(numbers))
 
+  # make sure the value is unicode
   decoded_val = val if isinstance(val, str) else val.decode(encode)
   token = Tokenized(decoded_val)
 
+  # iterate over value
   while token.has_next():
     kind, num = (token.kind, token.num_of_kind)
-
+    
     if kind == UNIT_KIND and token.last_kind != UNIT_KIND:
       ret = (numbers[-1] if numbers else 1) * num
       if numbers:
@@ -59,9 +58,10 @@ def to_arabic(val, encode='utf8'):
       token.next(incr=s)
 
     elif analyzing:
+
       _append_result()
       stacks, numbers, texts, analyzing, index = ([], [], [], False, -1)
-
+      
     analyzing = kind is not None
 
     if analyzing:
@@ -71,14 +71,23 @@ def to_arabic(val, encode='utf8'):
         index = token.pos
 
     if kind != NUMERIC_KIND:
-      next(token)
+      token.next()
 
   if stacks or numbers:
     _append_result()
-
+  
   return results
 
 
 def to_arabic_numbers(val, encode='utf8'):
   return tuple(x.number for x in to_arabic(val, encode=encode))
 
+
+
+def demo():
+  nums= to_arabic('銀河の向こう、六千三百二十一億千五百十一万二千百八十一光年彼方。')
+  print(nums)
+    
+  
+if __name__ == "__main__":
+  demo()
